207a4a59a9eb6659683db36fb70806a3f00992ec
ahamed-azaf <ahamedazaf01@gmail.com>
2025-04-21 21:24:45 +0530

    First commit

---
+++ access_modifire.php
@@ -0,0 +1,20 @@
+<?php
+
+class AccessModifire {
+
+    public function privateMethoud() {
+        echo "Hello";
+    }
+}
+
+class ChildAccessModifire extends AccessModifire {
+    public function accessprivetmethoud() {
+        $this->privateMethoud(); 
+    }
+}
+
+$obj = new ChildAccessModifire();
+$obj->accessprivetmethoud();
+
+
+?>
